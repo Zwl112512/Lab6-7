4fbabf3663ceb2d60e95752d5875e3e6a572ab45
Zwl112512 <147674826+Zwl112512@users.noreply.github.com>
2025-04-16 12:03:59 +0800

    Create config.ts

---
+++ config.ts
@@ -0,0 +1,9 @@
+export const config = {
+    host: 'your-db-host',
+    port: 5432,
+    user: 'your-db-username',
+    password: 'your-db-password',
+    database: 'your-db-name',
+    connection_limit: 100
+  }
+  
